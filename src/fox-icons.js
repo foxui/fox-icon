e96a1cd3b425b0be9b85f387a2abdab82adee527
miller <chen.minliang@gmail.com>
2014-06-03 15:45:51 +0800

    change prefix name

--- src/fox-icons.js
+++ src/fox-icons.js
@@ -3,7 +3,7 @@
     var icons = ['icon-back', 'icon-bars', 'icon-caret', 'icon-check', 'icon-close', 'icon-code', 'icon-compose', 'icon-download', 'icon-edit', 'icon-forward', 'icon-gear', 'icon-home', 'icon-info', 'icon-list', 'icon-more-vertical', 'icon-more', 'icon-pages', 'icon-pause', 'icon-person', 'icon-play', 'icon-plus', 'icon-refresh', 'icon-search', 'icon-share', 'icon-sound', 'icon-sound2', 'icon-sound3', 'icon-sound4', 'icon-star-filled', 'icon-star', 'icon-stop', 'icon-trash', 'icon-up-nav', 'icon-up', 'icon-right-nav', 'icon-right', 'icon-down-nav', 'icon-down', 'icon-left-nav', 'icon-left'];
 
     icons.forEach(function(icon) {
-        fox('w-' + icon, {
+        fox('fx-' + icon, {
             lifecycle: {
                 created: function() {
                     this.className = 'icon ' + icon;
